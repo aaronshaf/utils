503481316e187ccb05784c562200123ec43baff0
Aaron Shafovaloff <aaronshaf@gmail.com>
2018-08-11 11:43:44 -0600

    Update symbol-constants.js

--- symbol-constants.js
+++ symbol-constants.js
@@ -1,6 +1,6 @@
 // http://2ality.com/2018/08/enums-via-proxies.html
 export default const keyProxy = new Proxy({}, {
   get(_target, propKey, _receiver) {
-    return Symbol(propKey); // (A)
+    return Symbol(propKey);
   }
 });
